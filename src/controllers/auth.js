193c2b7756b17dfb9b52d789c409bbeb14b615cf
younggyo <lee4519698@hanmail.net>
2022-06-28 19:19:50 +0900

    [server] checkout

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,19 +1,18 @@
 // call services, respond to client requests
-const { AuthService } = require('../services');
-const { UserDto } = require('../dto/user');
+const { AuthService } = require("../services");
+const { UserDto } = require("../dto/user");
 
 class AuthController {
-    signup = async (req, res, next) => {
-        try{
+    static signup = async (req, res, next) => {
+        try {
             const userDto = new UserDto(req.body);
             const result = await AuthService.signup(userDto);
-    
+
             return res.status(201).send(result);
         } catch (err) {
             next(err);
         }
     };
 }
-
 
 module.exports = { AuthController };
